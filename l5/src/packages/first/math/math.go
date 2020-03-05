748a65b37921456811a1c851eb243a41020735b9
litleleprikon <e.sharifullin@skbkontur.ru>
2020-03-05 22:34:53 +0300

    feat(l5): Add lesson 5 slides

---
+++ l5/src/packages/first/math/math.go
@@ -0,0 +1,10 @@
+package math // HL01
+
+// Average is a function that count average between numbers
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
+}
